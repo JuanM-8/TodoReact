fc2301ea276e0308674fd984fd9d899186c8ff7b
Juan Marin <juandavidmarin.0008@gmail.com>
2025-11-02 23:36:35 -0500

    an image was added in footer

---
+++ src/assets/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer>
+      <div className="img-footer">
+        <img src="/mm.avif" alt="" />
+        Desarrollado por: Juan Marin
+      </div>
+    </footer>
+  );    
+}
